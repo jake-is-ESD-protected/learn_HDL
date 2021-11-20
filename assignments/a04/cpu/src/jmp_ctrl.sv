5567a6837ba0d141e0971d8e55b9f75346684c6a
jake-is-ESD-protected <jts4681@students.fhv.at>
2021-11-20 14:52:04 +0100

    fixed merging issues

--- assignments/a04/cpu/src/jmp_ctrl.sv
+++ assignments/a04/cpu/src/jmp_ctrl.sv
@@ -8,12 +8,6 @@ version:            V1.0
 *****************************************************************************************
 */
 
-<<<<<<< HEAD
-=======
-`define HIGH    (1'b1)
-`define LOW     (1'b0)
-
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
 module jmp_ctrl
 (
     input logic cmd_j1,
@@ -50,85 +44,50 @@ always_comb begin
         // covers JGT
         3'b001: begin
             if((~alu_ng) && (~alu_zr)) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
         end
         // covers JEQ
         3'b010: begin
             if(alu_zr) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
             
         end
         // covers JGE
         3'b011: begin
             if((~alu_ng) || alu_zr) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
         end
         // covers JLT
         3'b100: begin
             if(alu_ng) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
         end
         // covers JNE
         3'b101: begin
             if(~alu_zr) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
         end
         // covers JLE
         3'b110: begin
             if(alu_ng || alu_zr) begin
-<<<<<<< HEAD
                 pc_load = 1'b1;
                 pc_inc = 1'b0;
-=======
-                pc_load = `HIGH;
-                pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
             end
         end
         // covers JMP
         3'b111: begin
-<<<<<<< HEAD
             pc_load = 1'b1;
             pc_inc = 1'b0;
-=======
-            pc_load = `HIGH;
-            pc_inc = `LOW;
->>>>>>> 9637dd6dea5d49e49c920a177d42ae12a961f6a1
         end
     endcase
 end
